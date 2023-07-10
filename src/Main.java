06d23de382d6263759db90c6a5bdba2674dccd1f
Guilherme Silveira <lima.guilhermerj@gmail.com>
2023-07-10 20:52:03 -0300

    Adição de menu e mais atributos

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,58 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 import sistema.Produtos;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+        int option;
 
-        System.out.print("Qual a descrição do novo produto: ");
-        String descricao = scanner.nextLine();
+        System.out.println("-- PEDIDO DE VENDAS --");
 
-        System.out.print("Qual a ID do novo produto: ");
-        int id = scanner.nextInt();
 
-        Produtos newProduct = new Produtos(id, descricao);
-        /* newProduct.setId(id);
-         newProduct.setDescricao(description); */
+        do {
+            System.out.println("1 - Cadastrar produto");
+            System.out.println("0 - Sair");
 
-        System.out.println("Produto Criado com Sucesso!");
-        System.out.println("ID: " + newProduct.getId());
-        System.out.println("Descrição: " + newProduct.getDescricao());
+            System.out.print("Qual operação você deseja realizar: ");
+            option = scanner.nextInt();
 
+            process(option);
+
+        } while (option != 0);
+
+
+    }
+
+    public static void process(int option) throws Exception {
+        switch (option) {
+            case 1: {
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.print("Qual a descrição do novo produto: ");
+                String descricao = scanner.nextLine();
+
+                System.out.print("Qual a ID do novo produto: ");
+                int id = scanner.nextInt();
+
+                System.out.print("Qual o preço do produto: ");
+                double preco = scanner.nextDouble();
+
+                System.out.print("Qual a data de validade do produto: ");
+                String dataString = scanner.next();
+
+                Date dataValidade = new SimpleDateFormat("dd/MM/yyyy").parse(dataString);
+
+                Produtos newProduct = new Produtos(id, descricao, preco, dataValidade);
+                System.out.println("Produto Criado com Sucesso!");
+                System.out.println("ID: " + newProduct.getId());
+                System.out.println("Descrição: " + newProduct.getDescricao());
+                System.out.println("Preço: " + newProduct.getPreco());
+                System.out.println("Data de validade: " + newProduct.getDataValidade());
+                System.out.println("--------------------------------");
+            }
         }
     }
+}
--- src/sistema/Produtos.java
+++ src/sistema/Produtos.java
@@ -1,12 +1,18 @@
 package sistema;
 
+import java.util.Date;
+
 public class Produtos {
     private int id;
     private String descricao;
+    private double preco;
+    private Date dataValidade;
 
-    public Produtos(int id, String descricao) {
+    public Produtos(int id, String descricao, double preco, Date dataValidade) {
         this.id = id;
         this.descricao = descricao;
+        this.preco = preco;
+        this.dataValidade = dataValidade;
     }
 
     public void setId(int id) {
@@ -23,5 +29,13 @@ public class Produtos {
 
     public String getDescricao() {
         return descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public Date getDataValidade() {
+        return dataValidade;
     }
 }
